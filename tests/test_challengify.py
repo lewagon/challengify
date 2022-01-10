a07a6e9a32f68988a0f5a3c9b02644936b2310ca
Gaetan Manchon <gmanchon@gmail.com>
2022-01-10 16:12:42 +0100

    add boilerplate for tests

---
+++ tests/test_challengify.py
@@ -0,0 +1,27 @@
+
+import unittest
+
+
+class TestNotebookActions(unittest.TestCase):
+
+    def test_clean_notebook_output(self):
+        pass
+
+    def test_clean_notebook_stderr(self):
+        pass
+
+    def test_clean_cells_with_clean_tag(self):
+        pass
+
+    def test_delete_cells_with_delete_tag_(self):
+        pass
+
+    def test_delete_content_surrounded_by_delete_delimiters(self):
+        pass
+
+    def test_replace_content_surrounded_by_challengify_delimiters(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

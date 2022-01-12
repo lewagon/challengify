b7466a12024e7e82f726312f7df7bdd5939486b9
Gaetan Manchon <gmanchon@gmail.com>
2022-01-12 10:25:14 +0100

    correct test function name

--- tests/test_challengify.py
+++ tests/test_challengify.py
@@ -13,7 +13,7 @@ class TestNotebookActions(unittest.TestCase):
     def test_clean_cells_with_clean_tag(self):
         pass
 
-    def test_delete_cells_with_delete_tag_(self):
+    def test_delete_cells_with_delete_tag(self):
         pass
 
     def test_delete_content_surrounded_by_delete_delimiters(self):

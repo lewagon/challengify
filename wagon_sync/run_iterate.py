8a9f1668128de6de6e90a557ba420c001ef2bad4
Gaetan Manchon <gmanchon@gmail.com>
2022-01-19 10:40:08 +0100

    correct only on

--- wagon_sync/run_iterate.py
+++ wagon_sync/run_iterate.py
@@ -97,7 +97,7 @@ def process_ignored_files(source, version, only_to, only_on, only_from, verbose)
     ignored += [file for rule_version, files in only_to.items() for file in files if version > rule_version]
 
     # the current version is strictly equal to the version of the rule
-    ignored += [file for rule_version, files in only_on.items() for file in files if version == rule_version]
+    ignored += [file for rule_version, files in only_on.items() for file in files if version != rule_version]
 
     # the current version is strictly before the version of the rule
     ignored += [file for rule_version, files in only_from.items() for file in files if version < rule_version]

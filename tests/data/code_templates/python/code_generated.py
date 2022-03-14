6c299804fbc2595fde921ee83fbd5bd537a59cc1
Gaetan Manchon <gmanchon@gmail.com>
2022-03-14 12:22:35 +0100

    add code test files

---
+++ tests/data/code_templates/python/code_generated.py
@@ -0,0 +1,23 @@
+""" this is a sample python challenge file """
+
+import pandas as pd
+
+
+
+
+def function():
+    """
+    return the answer to life the universe and everything
+    """
+    pass  # YOUR CODE HERE
+
+
+this_is_not_removed = True
+
+
+
+this_is_not_removed_either = True
+
+# inline replacement sample
+if __name__ == '__main__':
+    pass  # YOUR CODE HERE

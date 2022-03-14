f7381c8c75dd1ca6f600ca8722b0277f5f2053fa
Gaetan Manchon <gmanchon@gmail.com>
2022-03-14 12:15:00 +0100

    rename notebook tests

--- tests/test_challengify.py
+++ tests/test_challengify.py
@@ -46,43 +46,43 @@ class TestNotebookActions(unittest.TestCase):
         clean the output of all notebook cells
         notebook has a filled metadata
         """
-        self.__transformation_test("test_clean_notebook_output")
+        self.__transformation_test("test_nb_clean_notebook_output")
 
     def test_clean_cleaned_notebook_output(self):
         """
         clean the output of all notebook cells
         notebook metadata has been cleaned by nbcleanmeta
         """
-        self.__transformation_test("test_clean_cleaned_notebook_output")
+        self.__transformation_test("test_nb_clean_cleaned_notebook_output")
 
     def test_clean_notebook_stderr(self):
         """
         clean the stderr of all notebook cells
         notebook has a filled metadata
         """
-        self.__transformation_test("test_clean_notebook_stderr")
+        self.__transformation_test("test_nb_clean_notebook_stderr")
 
     def test_clean_cleaned_notebook_stderr(self):
         """
         clean the stderr of all notebook cells
         notebook metadata has been cleaned by nbcleanmeta
         """
-        self.__transformation_test("test_clean_cleaned_notebook_stderr")
+        self.__transformation_test("test_nb_clean_cleaned_notebook_stderr")
 
     def test_clean_cells_with_clean_tag(self):
         """
         only clean the output and stderr of cells with corresponding tag
         """
-        self.__transformation_test("test_clean_cells_with_clean_tag")
+        self.__transformation_test("test_nb_clean_cells_with_clean_tag")
 
     def test_delete_cells_with_delete_tag(self):
-        self.__transformation_test("test_delete_cells_with_delete_tag")
+        self.__transformation_test("test_nb_delete_cells_with_delete_tag")
 
     def test_delete_content_surrounded_by_delete_delimiters(self):
-        self.__transformation_test("test_delete_content_surrounded_by_delete_delimiters")
+        self.__transformation_test("test_nb_delete_content_surrounded_by_delete_delimiters")
 
     def test_replace_content_surrounded_by_challengify_delimiters(self):
-        self.__transformation_test("test_replace_content_surrounded_by_challengify_delimiters")
+        self.__transformation_test("test_nb_replace_content_surrounded_by_challengify_delimiters")
 
 
 if __name__ == '__main__':

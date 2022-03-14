40269ad1c459629133af821b5d84f8318d643d72
Gaetan Manchon <gmanchon@gmail.com>
2022-03-14 14:36:17 +0100

    rename test notebooks

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -20,14 +20,14 @@ class TestNotebookActions(unittest.TestCase):
         # caller_name = inspect.currentframe().f_back.f_code.co_name
 
         # get notebook path
-        in_nb = os.path.join(os.path.dirname(__file__), "data", caller_name, "notebook_in.ipynb")
-        processed_nb = os.path.join(os.path.dirname(__file__), "data", caller_name, "notebook_challengify.ipynb")
+        in_nb = os.path.join(os.path.dirname(__file__), "notebooks", caller_name, "notebook_in.ipynb")
+        processed_nb = os.path.join(os.path.dirname(__file__), "notebooks", caller_name, "notebook_challengify.ipynb")
 
         # format python code
         process_notebook(in_nb, processed_nb, ".ipynb")
 
         # load notebooks
-        out_nb = os.path.join(os.path.dirname(__file__), "data", caller_name, "notebook_out.ipynb")
+        out_nb = os.path.join(os.path.dirname(__file__), "notebooks", caller_name, "notebook_out.ipynb")
 
         with open(out_nb, "r") as file:
             out_data = file.read()
@@ -50,43 +50,43 @@ class TestNotebookActions(unittest.TestCase):
         clean the output of all notebook cells
         notebook has a filled metadata
         """
-        self.__transformation_test("test_nb_clean_notebook_output")
+        self.__transformation_test("clean_notebook_output")
 
     def test_clean_cleaned_notebook_output(self):
         """
         clean the output of all notebook cells
         notebook metadata has been cleaned by nbcleanmeta
         """
-        self.__transformation_test("test_nb_clean_cleaned_notebook_output")
+        self.__transformation_test("clean_cleaned_notebook_output")
 
     def test_clean_notebook_stderr(self):
         """
         clean the stderr of all notebook cells
         notebook has a filled metadata
         """
-        self.__transformation_test("test_nb_clean_notebook_stderr")
+        self.__transformation_test("clean_notebook_stderr")
 
     def test_clean_cleaned_notebook_stderr(self):
         """
         clean the stderr of all notebook cells
         notebook metadata has been cleaned by nbcleanmeta
         """
-        self.__transformation_test("test_nb_clean_cleaned_notebook_stderr")
+        self.__transformation_test("clean_cleaned_notebook_stderr")
 
     def test_clean_cells_with_clean_tag(self):
         """
         only clean the output and stderr of cells with corresponding tag
         """
-        self.__transformation_test("test_nb_clean_cells_with_clean_tag")
+        self.__transformation_test("clean_cells_with_clean_tag")
 
     def test_delete_cells_with_delete_tag(self):
-        self.__transformation_test("test_nb_delete_cells_with_delete_tag")
+        self.__transformation_test("delete_cells_with_delete_tag")
 
     def test_delete_content_surrounded_by_delete_delimiters(self):
-        self.__transformation_test("test_nb_delete_content_surrounded_by_delete_delimiters")
+        self.__transformation_test("delete_content_surrounded_by_delete_delimiters")
 
     def test_replace_content_surrounded_by_challengify_delimiters(self):
-        self.__transformation_test("test_nb_replace_content_surrounded_by_challengify_delimiters")
+        self.__transformation_test("replace_content_surrounded_by_challengify_delimiters")
 
 
 if __name__ == '__main__':

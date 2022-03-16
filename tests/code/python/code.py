faf74544e653ce84e718093997483340b6d5c036
Gaetan Manchon <gmanchon@gmail.com>
2022-03-16 14:49:03 +0100

    add tests boilerplate for new delimiters

--- tests/code/python/code.py
+++ tests/code/python/code.py
@@ -16,13 +16,56 @@ def function():
     # $CHALLENGIFY_END
 
 
+def new_cha_delimiter():
+    # $CHA_BEGIN
+    return 42
+    # $CHA_END
+
+
 this_is_not_removed = True
 
 # $DELETE_BEGIN
 this_is_removed = True
 # $DELETE_END
 
+# $DEL_BEGIN
+new_del_delimiter = True
+# $DEL_END
+
 this_is_not_removed_either = True
+
+# the delete delimiters do not consume the line of the block
+# before the block
+# $DEL_BEGIN
+# $DEL_END
+# after the block
+
+# the erase delimiters consume the line of the block
+# before the block
+# $ERASE_BEGIN
+tralala = True
+# $ERASE_END
+# after the block
+
+# the wipe delimiters consume the line of the block and the line after
+# in order to keep outer blocks evenly spaced
+# before the block
+
+# $WIPE_BEGIN
+trololo = True
+# $WIPE_END
+
+# after the block
+
+# the implode delimiters consume the line of the block and the lines around the block
+# in order to remove outer blocks spacing
+# before the block
+
+# $IMPLODE_BEGIN
+trilili = True
+# $IMPLODE_END
+
+# after the block
 
 # inline replacement# $DELETE_BEGIN for code or text # $DELETE_END sample
 if __name__ == '__main__':
--- tests/code/python/code_generated.py
+++ tests/code/python/code_generated.py
@@ -12,11 +12,54 @@ def function():
     pass  # YOUR CODE HERE
 
 
+def new_cha_delimiter():
+    # $CHA_BEGIN
+    return 42
+    # $CHA_END
+
+
 this_is_not_removed = True
 
 
 
+# $DEL_BEGIN
+new_del_delimiter = True
+# $DEL_END
+
 this_is_not_removed_either = True
+
+# the delete delimiters do not consume the line of the block
+# before the block
+# $DEL_BEGIN
+# $DEL_END
+# after the block
+
+# the erase delimiters consume the line of the block
+# before the block
+# $ERASE_BEGIN
+tralala = True
+# $ERASE_END
+# after the block
+
+# the wipe delimiters consume the line of the block and the line after
+# in order to keep outer blocks evenly spaced
+# before the block
+
+# $WIPE_BEGIN
+trololo = True
+# $WIPE_END
+
+# after the block
+
+# the implode delimiters consume the line of the block and the lines around the block
+# in order to remove outer blocks spacing
+# before the block
+
+# $IMPLODE_BEGIN
+trilili = True
+# $IMPLODE_END
+
+# after the block
 
 # inline replacement sample
 if __name__ == '__main__':

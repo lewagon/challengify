8b1f99b89b3d838487e74d90ddce51a14e365e7c
Gaetan Manchon <gmanchon@gmail.com>
2022-03-16 10:34:08 +0100

    add tests for indented content

--- tests/unit/test_code_edition.py
+++ tests/unit/test_code_edition.py
@@ -33,3 +33,33 @@ class TestCodeEdition():
         assert replaced_content == expected_result
 
         # Cleanup
+
+    def test_indented_content_replacement(self):
+        """
+        # test that exec count integers are present when no action is performed
+        """
+        # Arrange
+        source = """
+            before
+                $BEGIN
+                content
+                $END
+            after
+        """
+        replacement = "replacement"
+        begin = "$BEGIN"
+        end = "$END"
+
+        expected_result = """
+            before
+                replacement
+            after
+        """
+
+        # Act
+        replaced_content = replace_content(source, replacement, begin, end)
+
+        # Assert
+        assert replaced_content == expected_result
+
+        # Cleanup

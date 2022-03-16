c947969e3f289f0d4861b97f9e5a40736f4bde10
Gaetan Manchon <gmanchon@gmail.com>
2022-03-16 11:27:41 +0100

    rename code edition tests

--- tests/unit/test_code_edition_replacement.py
+++ tests/unit/test_code_edition_replacement.py
@@ -2,9 +2,12 @@
 from wagon_sync.code_edition import replace_content
 
 
-class TestCodeEdition():
+class TestCodeEditionReplacement():
 
     def test_replacement(self):
+        """
+        test inline unindented code replacement
+        """
         # Arrange
         source = """
             before
@@ -32,6 +35,9 @@ class TestCodeEdition():
         # Cleanup
 
     def test_indented_replacement(self):
+        """
+        test inline indented code replacement
+        """
         # Arrange
         source = """
             before
@@ -47,60 +53,6 @@ class TestCodeEdition():
         expected_result = """
             before
                 pass
-            after
-        """
-
-        # Act
-        replaced_content = replace_content(source, replacement, begin, end)
-
-        # Assert
-        assert replaced_content == expected_result
-
-        # Cleanup
-
-    def test_suppression(self):
-        # Arrange
-        source = """
-before
-$BEGIN
-content
-$END
-after
-        """
-        replacement = ""
-        begin = "$BEGIN"
-        end = "$END"
-
-        expected_result = """
-before
-
-after
-        """
-
-        # Act
-        replaced_content = replace_content(source, replacement, begin, end)
-
-        # Assert
-        assert replaced_content == expected_result
-
-        # Cleanup
-
-    def test_indented_suppression(self):
-        # Arrange
-        source = """
-            before
-                $BEGIN
-                content
-                $END
-            after
-        """
-        replacement = ""
-        begin = "$BEGIN"
-        end = "$END"
-
-        expected_result = """
-            before
-""" + "                " + """
             after
         """
 
--- tests/unit/test_code_edition_suppression.py
+++ tests/unit/test_code_edition_suppression.py
@@ -2,7 +2,7 @@
 from wagon_sync.code_edition import replace_content
 
 
-class TestUnindentedCodeEdition():
+class TestCodeEditionSuppression():
 
     def test_unindented_suppression(self):
         """
@@ -160,6 +160,37 @@ after
         expected_result = """
 before
 after
+        """
+
+        # Act
+        replaced_content = replace_content(source, replacement, begin, end)
+
+        # Assert
+        assert replaced_content == expected_result
+
+        # Cleanup
+
+    def test_indented_suppression(self):
+        """
+        test that indented suppression
+        leaves in place a blank line with undesired indentation
+        """
+        # Arrange
+        source = """
+            before
+                $BEGIN
+                content
+                $END
+            after
+        """
+        replacement = ""
+        begin = "$BEGIN"
+        end = "$END"
+
+        expected_result = """
+            before
+""" + "                " + """
+            after
         """
 
         # Act

d8a04c95f82d8c065c3ec3e963b050a7dddaf0b5
Gaetan Manchon <gmanchon@gmail.com>
2022-03-20 11:11:44 +0100

    split test file

--- tests/unit/test_code_suppression_indented.py
+++ tests/unit/test_code_suppression_indented.py
@@ -2,7 +2,7 @@
 from wagon_sync.code_edition import replace_content
 
 
-class TestCodeEditionSuppression():
+class TestCodeSuppressionUnindented():
 
     def test_unindented_delete(self):
         """
@@ -160,37 +160,6 @@ after
         expected_result = """
 before
 after
-        """
-
-        # Act
-        replaced_content = replace_content(source, replacement, begin, end)
-
-        # Assert
-        assert replaced_content == expected_result
-
-        # Cleanup
-
-    def test_indented_delete(self):
-        """
-        test that indented suppression
-        leaves in place a blank line with undesired indentation
-        """
-        # Arrange
-        source = """
-            before
-                $BEGIN
-                content
-                $END
-            after
-        """
-        replacement = ""
-        begin = "$BEGIN"
-        end = "$END"
-
-        expected_result = """
-            before
-""" + "                " + """
-            after
         """
 
         # Act

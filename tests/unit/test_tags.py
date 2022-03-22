afb0e59043d0031e711700984dfd1f6d3e49da61
Gaetan Manchon <gmanchon@gmail.com>
2022-03-22 17:24:20 +0100

    add tag class and test

---
+++ tests/unit/test_tags.py
@@ -0,0 +1,38 @@
+
+from wagon_sync.tag import Tag
+
+
+class TestTags():
+
+    def test_tag_apply(self):
+        """
+        verify that the content is correctly transformed
+        when a tag is applied on it
+        """
+
+        # Arrange
+        tag = Tag(
+            begin="# $CHA_BEGIN",
+            end="# $CHA_END",
+            replacement="# pass",
+            eat_indentation=False)
+
+        # Act
+        decorated = tag.apply("""
+            block before
+                # $CHA_BEGIN
+                some content inside
+                # $CHA_END# block end
+            block after
+            """)
+
+        expected = """
+            block before
+                # pass# block end
+            block after
+            """
+
+        # Assert
+        assert decorated == expected
+
+        # Cleanup
--- wagon_sync/tag.py
+++ wagon_sync/tag.py
@@ -1,12 +1,22 @@
 
-class Tag:
+from wagon_sync.code_edition import replace_content
 
-    def __init__(self, begin, end, fill, eat_indentation):
 
-        self.begin = begin
-        self.end = end
-        self.fill = fill
+class Tag:  # Mask
+
+    def __init__(self, begin, end, replacement, eat_indentation):
+
+        self.begin = begin                        # begin delimiter
+        self.end = end                            # end delimiter
+        self.replacement = replacement
         self.eat_indentation = eat_indentation
+
+    def apply(self, content):
+
+        decorated = replace_content(
+            content, self.replacement, self.begin, self.end, self.eat_indentation)
+
+        return decorated
 
     def print(self):
 
@@ -15,5 +25,5 @@ class Tag:
 
         print(f"\n- begin: {begin}"
               + f"\n- end: {end}"
-              + f"\n- fill: {self.fill}"
+              + f"\n- replacement: {self.replacement}"
               + f"\n- eat_indentation: {self.eat_indentation}")

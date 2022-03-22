89378189b72cd34da9f948b01c2d15c177e25325
Gaetan Manchon <gmanchon@gmail.com>
2022-03-22 18:10:03 +0100

    comment

--- tests/unit/test_verbs.py
+++ tests/unit/test_verbs.py
@@ -5,6 +5,9 @@ from wagon_sync.verbs import Verbs
 class TestVerbs():
 
     def test_print_verbs(self):
+        """
+        visual test (use `pytest -s`)
+        """
 
         # Arrange
         verbs = Verbs()

ac02d033ae3be4a07f65c9a276f5856ef9c0a2db
Gaetan Manchon <gmanchon@gmail.com>
2022-03-23 15:45:09 +0100

    clarify run functional test variables naming

--- tests/functional/run/test_code_base.py
+++ tests/functional/run/test_code_base.py
@@ -26,8 +26,8 @@ class TestCodeBase(unittest.TestCase):
         # get data path
         in_file = os.path.join(os.path.dirname(__file__), "..", "..", "data", "code", source_file)
         in_file_root, in_file_extension = os.path.splitext(in_file)
-        out_file = f"{in_file_root}_generated{in_file_extension}"
-        processed_file = f"{in_file_root}_processed{in_file_extension}"
+        out_file = f"{in_file_root}_processed{in_file_extension}"
+        control_file = f"{in_file_root}_generated{in_file_extension}"
 
         # process file extension
         file_extension = get_file_extension(in_file)
@@ -35,17 +35,17 @@ class TestCodeBase(unittest.TestCase):
         # transform code
         process_code(
             self.challengify,
-            in_file, processed_file, file_extension)
+            in_file, out_file, file_extension)
 
         # read files
-        with open(out_file, "r") as file:
+        with open(control_file, "r") as file:
             out_content = file.read()
 
-        with open(processed_file, "r") as file:
+        with open(out_file, "r") as file:
             processed_content = file.read()
 
         # remove processed file
-        os.remove(processed_file)
+        os.remove(out_file)
 
         # compare results
         self.assertEqual(out_content, processed_content)

4d6458df33fbb8ed26908dd2c5d01e2548d0b4c4
Gaetan Manchon <gmanchon@gmail.com>
2022-03-24 12:51:19 +0100

    add iterate functional test

--- tests/functional/iterate/test_iterate.py
+++ tests/functional/iterate/test_iterate.py
@@ -6,6 +6,27 @@ import unittest
 
 import os
 
+from wagon_common.helpers.subprocess import run_command
+
+from colorama import Fore, Style
+
+
+def are_directories_identical(directory_a, directory_b):
+    """
+    recursively compare directory structure and file content
+    """
+
+    # compare directories
+    command = [
+        "diff",
+        "-r",
+        directory_a,
+        directory_b]
+
+    rc, output, error = run_command(command, verbose=False)
+
+    return rc, output, error
+
 
 class TestIterate(unittest.TestCase):
     """
@@ -17,23 +38,41 @@ class TestIterate(unittest.TestCase):
         # Arrange
         challengify = Challengify()
 
-        source_path = os.path.join(
+        in_path = os.path.join(
             os.path.dirname(__file__), "..", "..", "data", "iterate", "source")
+
+        out_path = os.path.join(
+            os.path.dirname(__file__), "..", "..", "data", "iterate", "processed")
+
+        control_path = os.path.join(
+            os.path.dirname(__file__), "..", "..", "data", "iterate", "control")
 
         # Act
         run_iterate(
             challengify=challengify,
-            source=source_path,
+            source=in_path,
             min_version=None,
             max_version=None,
             force=True,
             dry_run=False,
-            verbose=True,
+            verbose=False,
             ignore_metadata=False,
             format=False)
 
         # Assert
-        assert False
+        rc, output, error = are_directories_identical(out_path, control_path)
+
+        if rc != 0:
+
+            print(Fore.RED
+                  + "\nDirectories content does not match 🤕"
+                  + Style.RESET_ALL
+                  + f"\n- rc: {rc}"
+                  + f"\n- output: {output}"
+                  + f"\n- error: {error}")
+            print(output.decode("utf-8"))
+
+        assert rc == 0
 
         # Cleanup
 

7a87af7922fe9c6cb65d29414580affedce48dad
Gaetan Manchon <gmanchon@gmail.com>
2022-04-08 00:40:48 +0200

    cache file delimiters search to improve speed on iterations

--- wagon_sync/file.py
+++ wagon_sync/file.py
@@ -1,10 +1,18 @@
 
+from wagon_sync.language_decorators import LanguageDecorators
 from wagon_sync.decorator import Decorator
+
+import re
 
 
 class File:
 
+    # TODO refacto
     cached_content = {}
+    cached_delimiters = {}
+
+    # TODO refacto
+    decorators = LanguageDecorators()
 
     def __init__(self, source, destination, language):
 
@@ -13,6 +21,10 @@ class File:
         self.language = language
 
         self.content = self.__load(self.source)
+
+        self.decorator = self.decorators.get(self.language)
+
+        self.delimiters = self.__load_delimiters(self.content)
 
     def __load(self, source):
         """
@@ -32,10 +44,37 @@ class File:
 
         return content
 
+    def __load_delimiters(self, content):
+        """
+        scan file for delimiters
+        """
+
+        delimiters = []
+
+        # TODO
+        # self.decorator.tag_pattern
+
+        # load cached delimiters
+        if self.source in self.cached_delimiters:
+            return self.cached_delimiters[self.source]
+
+        # search for delimiters
+        matches = re.search("(# \\$.*_BEGIN)|(# \\$.*_END)", content)
+
+        delimiters = bool(matches)
+
+        # cache delimiters
+        self.cached_delimiters[self.source] = delimiters
+
+        return delimiters
+
     def decorate(self, decorator: Decorator):
 
         # decorate content
-        self.decorated = decorator.decorate(self.content)
+        if self.delimiters:
+            self.decorated = decorator.decorate(self.content)
+        else:
+            self.decorated = self.content
 
         # write destination
         with open(self.destination, "w") as file:

0c43cfc98500c0f6bbcbac976d7bd7479b2291ca
Gaetan Manchon <gmanchon@gmail.com>
2022-04-07 23:13:54 +0200

    cache file loading to improve speed on iterations

--- wagon_sync/file.py
+++ wagon_sync/file.py
@@ -4,15 +4,33 @@ from wagon_sync.decorator import Decorator
 
 class File:
 
+    cached_content = {}
+
     def __init__(self, source, destination, language):
 
         self.source = source
         self.destination = destination
         self.language = language
 
+        self.content = self.__load(self.source)
+
+    def __load(self, source):
+        """
+        cached file loader
+        """
+
+        # load cached content
+        if source in self.cached_content:
+            return self.cached_content[source]
+
         # read source
-        with open(self.source, 'r') as file:
-            self.content = file.read()
+        with open(source, 'r') as file:
+            content = file.read()
+
+        # cache content
+        self.cached_content[source] = content
+
+        return content
 
     def decorate(self, decorator: Decorator):
 

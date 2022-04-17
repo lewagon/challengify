da424f7edf941e3a681d2a3c8b5e453e27675a2a
Gaetan Manchon <gmanchon@gmail.com>
2022-04-17 17:45:52 +0200

    move are_directories_identical to wagon_common

--- tests/functional/iterate/test_iterate.py
+++ tests/functional/iterate/test_iterate.py
@@ -1,18 +1,13 @@
 
-from tests.functional.test_directories import are_directories_identical
-
 from wagon_sync.challengify import Challengify
 from wagon_sync.run_iterate import run_iterate
 
-import unittest
+from wagon_common.tests.base.directory_equality import TestBaseDirectoryEquality
 
 import os
-import shutil
-
-from colorama import Fore, Style
 
 
-class TestIterate(unittest.TestCase):
+class TestIterate(TestBaseDirectoryEquality):
     """
     test that challenge versions are correctly generated from source codebase
     """
@@ -20,44 +15,28 @@ class TestIterate(unittest.TestCase):
     def test_iterate(self):
 
         # Arrange
+        tests_root = os.path.join("tests", "data")
+
         challengify = Challengify()
 
-        data_path = os.path.join("tests", "data", "iterate")
-
-        in_path = os.path.join(data_path, "source")
-        out_path = os.path.join(data_path, "processed")
-        control_path = os.path.join(data_path, "control")
-
         # Act
-        run_iterate(
-            challengify=challengify,
-            source=in_path,
-            min_version=None,
-            max_version=None,
-            force=True,
-            dry_run=False,
-            verbose=True,
-            ignore_metadata=False,
-            format=False)
+        def act():
+
+            run_iterate(
+                challengify=challengify,
+                source=self.source_root,
+                min_version=None,
+                max_version=None,
+                force=True,
+                dry_run=False,
+                verbose=True,
+                ignore_metadata=False,
+                format=False)
 
         # Assert
-        rc, output, error = are_directories_identical(out_path, control_path)
-
-        if rc != 0:
-
-            print(Fore.RED
-                  + "\nDirectory content does not match 🤕"
-                  + Style.RESET_ALL
-                  + f"\n- rc: {rc}"
-                  + f"\n- output: {output}"
-                  + f"\n- error: {error}")
-            print(output.decode("utf-8"))
-
-        assert rc == 0
+        self.run_test_directory_identical(
+            os.path.join(
+                tests_root, "iterate"),
+            act)
 
         # Cleanup
-        shutil.rmtree(out_path, ignore_errors=True)
-
-
-if __name__ == '__main__':
-    unittest.main()
